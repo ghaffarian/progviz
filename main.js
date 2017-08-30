8be4fa3b82fa311c3e9ce7288292d2d5dfd88915
aliiz <aliizadi2030@gmail.com>
2017-08-30 13:40:04 +0430

    added menu bar

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 'use strict';
 
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, Menu} = require('electron')
 const url = require('url')
 const path = require('path')
 const {ipcMain} = require('electron')
 const electron = require('electron')
+
 
 
 let win
@@ -14,7 +15,7 @@ function createWindow() {
    var screenElectron = electron.screen;
    let mainScreen = screenElectron.getPrimaryDisplay();
    let dimensions = mainScreen.size;
-   win.setSize(dimensions.width, dimensions.height);
+   // win.setSize(dimensions.width, dimensions.height);
    win.loadURL(url.format({
       pathname: path.join(__dirname, 'index.html'),
       protocol: 'file:',
@@ -47,3 +48,83 @@ ipcMain.on('openFile', (event, path) => {
       })
       }
 }) 
+
+
+/*
+*
+menu
+*
+*/
+
+const template = [
+   {
+      label: 'File',
+      submenu: [
+         {
+            label: 'New',
+            accelerator: "ctrl+n"
+         },
+         {
+            label: 'Open'
+         },
+         {
+            label: 'Exit',
+            accelerator: "ctrl+q"
+         }
+      ]
+   },
+   {
+      label: 'View',
+      submenu: [
+         {
+            role: 'reload'
+         },
+         {
+            role: 'toggledevtools'
+         },
+         {
+            type: 'separator'
+         },
+         {
+            role: 'resetzoom'
+         },
+         {
+            role: 'zoomin'
+         },
+         {
+            role: 'zoomout'
+         },
+         {
+            type: 'separator'
+         },
+         {
+            role: 'togglefullscreen'
+         }
+      ]
+   },
+   {
+      role: 'window',
+      submenu: [
+         {
+            role: 'minimize'
+         },
+         {
+            role: 'close'
+         }
+      ]
+   },
+   {
+      role: 'help',
+      submenu: [
+         {
+            label: 'Learn More',
+         },
+        {
+            label: 'Check for updates'
+        }
+      ]
+   }
+]
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
