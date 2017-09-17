ba34b147e9803580892803f7da3f3e90786b097a
aliiz <aliizadi2030@gmail.com>
2017-09-17 11:53:37 +0430

    refactor code and added new feature

--- src/graphDrawer.js
+++ src/graphDrawer.js
@@ -1,33 +1,17 @@
 const dagreD3 = require('dagre-d3');
 const d3 = require('d3');
-const graphCreator = require('./src/graphCreator.js');
-const graphStyleUpdater = require('./src/graphStyleUpdater.js');
-const graphProperties = require('./src/graphProperties.js');
-
-let svg;
-
-function init() {
-    svg = d3.select("svg")
-}
+const graphCreator = require('./../src/graphCreator.js');
+const graphStyleUpdater = require('./../src/graphStyleUpdater.js');
+const graphProperties = require('./../src/graphProperties.js');
 
 
 function drawGraph(g) {
 
-    /*********** updating examples **************/
-    // g = graphStyleUpdater.change_node(g,"n30", "red");
-    // g = graphStyleUpdater.change_neighbours(g, "n32", "red");
-    // g = graphStyleUpdater.change_edge(g,"n32","n33", "blue");
-    // g = graphStyleUpdater.change_two_nodes_of_edge(g,"n32","n33", "red");
 
-    //svg.selectAll("*").remove();
+    let svg = d3.select("svg");
     let inner = svg.select("g");
 
-    // Set up zoom support
-    let zoom = d3.behavior.zoom().on("zoom", function () {
-        inner.attr("transform", "translate(" + d3.event.translate + ")" +
-            "scale(" + d3.event.scale + ")");
-    });
-    svg.call(zoom);
+    let w = 520, h = 600;
 
     // Create the renderer
     let render = new dagreD3.render();
@@ -35,9 +19,8 @@ function drawGraph(g) {
     // Run the renderer. This is what draws the final graph.
     render(inner, g);
 
-
-    let w = 520, h = 600;
     let colors = d3.scale.category20();
+
 
     let vis = d3.select("#chart").append("svg:svg")
         .attr("width", w)
@@ -56,6 +39,7 @@ function drawGraph(g) {
         .attr("cy", function (d, i) { return d.y; })
         .attr("r", function (d, i) { return d.r; });
 
+
     $('svg g.node').tipsy({
         gravity: 'w',
         html: true,
@@ -65,33 +49,91 @@ function drawGraph(g) {
         }
     });
 
+    graphStyleUpdater.change_node_color_onHover(svg);
+    zoom_by_scroll(svg, inner)
+    zoom_In_Out(w,h);
+
+
+}
+
+function zoom_by_scroll (svg, inner) { 
+    let zoom = d3.behavior.zoom().on("zoom", function () {
+        inner.attr("transform", "translate(" + d3.event.translate + ")" +
+            "scale(" + d3.event.scale + ")");
+    });
+    svg.call(zoom);
+}
+
+function zoom_In_Out  (w, h) {
+
+     var makePanZoomCTRL = function (width, height) {
+        var control = {}
+
+        var zoomMin = -10, // Levels of Zoom Out
+            zoomMax = 10, // Levels of Zoom In
+            zoomCur = 0; // Current Zoom
+
+
+        var transform = function () {
+            var x = -((width * zoomCur / 10) / 2);
+            var y = -((height * zoomCur / 10) / 2);
+            var s = (zoomCur / 10) + 1;
+            d3.select("g")
+                .attr("transform", "translate(" + x + " " + y + ") scale(" + s + ")");
+        };
+
+        control.zoom = function (btnID) {
+            if (btnID === "zoomIn") {
+                if (zoomCur >= zoomMax) return;
+                zoomCur++;
+            } else if (btnID === "zoomOut") {
+                if (zoomCur <= zoomMin) return;
+                zoomCur--;
+            }
+            transform();
+        };
+        return control;
+    }
+
+    //***********************************************************
+    // INSTANTIATE PAN-ZOOM CONTROL (CREATE INSTANCE)
+    //***********************************************************
+    var panZoom = makePanZoomCTRL(w, h);
+
+    //***********************************************************
+    // SET BUTTON EVENT LISTENERS
+    //***********************************************************
+    d3.selectAll("#zoomIn, #zoomOut")
+        .on("click", function () {
+            d3.event.preventDefault();
+            var id = d3.select(this).attr("id");
+            panZoom.zoom(id);
+        });
+ 
 
 
 }
 
 
-const { ipcRenderer } = require('electron')
-
-ipcRenderer.send('openFile', () => {
-    console.log("Event sent.");
-})
-ipcRenderer.on('fileData', (event, data) => {
+module.exports.draw = (data) => {
     graph_dot = data;
-    init();
     let g = graphCreator.getGraph(graph_dot, "TB", "ellipse");
+    properties(g);
     drawGraph(g);
-    //properties();
-
-})
-
-function properties() {
-
-    console.log(graphProperties.getNumberOfNodes());
-    document.getElementById('nodesProoerty').innerHTML = "Nodes: " + graphProperties.getNumberOfNodes();
-    document.getElementById('edgesProperty').innerHTML = "Edges: " + graphProperties.getNumberOfEdges();
-    document.getElementById('nameProperty').innerHTML = "Name: " +  graphProperties.getName();
 
 }
+
+function properties(g) {
+
+    // console.log(graphProperties.getNumberOfNodes());
+    document.getElementById('nodesProoerty').innerHTML = "Nodes: " + graphProperties.getNumberOfNodes(g);
+    document.getElementById('edgesProperty').innerHTML = "Edges: " + graphProperties.getNumberOfEdges(g);
+    document.getElementById('nameProperty').innerHTML = "Name: " + graphProperties.getName(g);
+
+}
+
+
+
 
 
 
