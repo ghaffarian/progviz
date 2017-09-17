dd9d64bad6c5fb9f9487ef88e7198bed80704986
aliiz <aliizadi2030@gmail.com>
2017-09-17 11:57:40 +0430

    edit change color when mouse hover on nodes

--- src/graphStyleUpdater.js
+++ src/graphStyleUpdater.js
@@ -1,41 +1,29 @@
 const dagreD3 = require('dagre-d3');
 
-module.exports.change_node = (g, node_id, color) => {
-    g.node(node_id).style = "fill : " + color;
-    return g;
-}
+module.exports.change_node_color_onHover = (svg) => {
 
-module.exports.change_edge = (g, sourceId, targetId, color) => {
-    console.log(g.edge(sourceId, targetId));
-    g.edge(sourceId, targetId).style = "stroke : " + color + ";" + " stroke-width:2px;";
-    return g;
-} 
+    svg.selectAll("g.node")
+        .on("mouseover", handleMouseOver)
+        .on("mouseout", handleMouseOut);
 
+    // Create Event Handlers for mouse
+    function handleMouseOver(d, i) {  // Add interactivity
 
-module.exports.change_neighbours = (g, node_id, color) => { 
+        // Use D3 to select element, change color and size
+        d3.select(this).attr({
+            fill: "yellow",
+            stroke : "yellow"
+        });
+    }
 
-    let inEdges = g.inEdges(node_id);
-    let outEdges = g.outEdges(node_id);
+    function handleMouseOut(d, i) {
+        // Use D3 to select element, change color back to normal
+        d3.select(this).attr({
+            fill: "black",
+            stroke : null
 
+        });
 
-    let neighbours = new Array();
+    }
 
-    for(let i=0; i<inEdges.length; i++)
-        neighbours.push(inEdges[i].v);
-
-    for(let i=0; i<outEdges.length; i++)
-        neighbours.push(outEdges[i].w);
-
-    console.log(neighbours);
-
-    for(let i=0; i<neighbours.length; i++)
-        g.node(neighbours[i]).style = "fill : " + color;
-
-    return g; 
-}
-
-module.exports.change_two_nodes_of_edge = (g, sourceId, targetId, color) => {
-    g.node(sourceId).style = "fill : " + color;
-    g.node(targetId).style = "fill : " + color;
-    return g; 
 }
