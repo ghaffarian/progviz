6347007ae9e32ab535664eb1e663c4333104e7ce
aliiz <aliizadi2030@gmail.com>
2017-09-17 11:50:38 +0430

    added bind functions for electron app

--- main.js
+++ main.js
@@ -12,42 +12,38 @@ let win
 
 function createWindow() {
    win = new BrowserWindow({width: 800, height: 600})
-   var screenElectron = electron.screen;
-   let mainScreen = screenElectron.getPrimaryDisplay();
-   let dimensions = mainScreen.size;
-   // win.setSize(dimensions.width, dimensions.height);
    win.loadURL(url.format({
       pathname: path.join(__dirname, 'index.html'),
       protocol: 'file:',
       slashes: true
    }))
+
+   // Emitted when the window is closed.
+    win.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        win = null
+    })
 }
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
-ipcMain.on('openFile', (event, path) => {
-      const {dialog} = require('electron')
-      const fs = require('fs')
-      dialog.showOpenDialog(function (fileNames) {
-            // fileNames is an array that contains all the selected
-          if(fileNames === undefined){
-                  console.log("No file selected");
-          }else{
-                  readFile(fileNames[0]);
-          }
-      });
+app.on('window-all-closed', () => {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
 
-      function readFile(filepath){
-         fs.readFile(filepath, 'utf-8', (err, data) => {
-            if(err){
-               alert("An error ocurred reading the file :" + err.message)
-               return
-            }
-            // handle the file content
-            event.sender.send('fileData', data)
-      })
-      }
-}) 
+app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+        createWindow()
+    }
+})
 
 
 /*
