724bcb0019e6a1cb0309f3a75988999cba01f603
aliiz <aliizadi2030@gmail.com>
2017-09-18 23:03:05 +0430

    added fuse.js for searching

---
+++ src/fuseSearch.js
@@ -0,0 +1,29 @@
+const fuseJs = require('fuse.js');
+
+
+module.exports.search = (g, query) => {
+
+
+    let nodes = new Array();
+
+    for (key in g._nodes) {
+        let node = {};
+        node['id'] = key;
+        node['label'] = g._nodes[key].label;
+        nodes.push(node);
+    }
+
+    let options = {
+        keys: ['label']
+    };
+    let fuse = new fuseJs(nodes, options)
+
+    let result = fuse.search(query);
+
+
+    for (let i=0 ; i<result.length; i++) {
+        g._nodes[result[i].id].style =  "fill : " + "yellow";
+    }
+    return g;
+
+} 
